a43a704c40e3d47968d23c4eb7414964edae9338
Julian Yaman <julianyaman@posteo.eu>
2019-03-11 20:54:22 +0100

    Begin of the project

---
+++ bot.js
@@ -0,0 +1,16 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+const config = require("./config")
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('pong');
+  }
+});
+
+client.login(config.token);
