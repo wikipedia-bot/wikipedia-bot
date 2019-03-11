5021db1e4724586cfbaf0e23b33eef576e9e6ff6
Julian Yaman <julianyaman@posteo.eu>
2019-03-11 21:10:58 +0100

    Adding first command -> help + added variable names for config properties

--- bot.js
+++ bot.js
@@ -12,7 +12,8 @@ try {
 }
 const client = new Discord.Client();
 
-const config = require("./config")
+var {PREFIX, VERSION, TOKEN} = require("./config")
+
 
 
 // Handling client events
@@ -22,23 +23,17 @@ client.on('error', console.error)
 
 client.on('ready', async () => {
   console.log('Starting Bot...\nNode version: ' + process.version + '\nDiscord.js version: ' + Discord.version + '\n')
-  console.log('This Bot is online! Running on version ' + config.version)
+  console.log('This Bot is online! Running on version ' + VERSION)
   client.user.setPresence({
     status: "online",
     game: {
-      name: `on ${client.guilds.size} servers! ${config.prefix}help`
+      name: `on ${client.guilds.size} servers! ${PREFIX}help`
     }
   }).catch(e => {
     console.error(e)
   })
   console.log(`Ready to serve on ${client.guilds.size} servers for a total of ${client.users.size} users.`)
 })
-
-client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('pong');
-  }
-});
 
 client.on('disconnect', () => console.log('I disconnected currently but I will try to reconnect!'))
 
@@ -68,4 +63,26 @@ client.on('guildDelete', guild => {
   })
 })
 
-client.login(config.token);
+client.on('message', async msg => {
+  if (msg.isMentioned(client.user)) {
+    msg.delete().catch(e => {
+      // console.error(e)
+      msg.channel.send('❌ Message to the owner of the server: **Please give the right permissions to me so I can delete this message.**')
+    })
+    msg.author.send({
+      embed: {
+        color: 3447003,
+        title: 'Wikipedia -> Commands',
+        fields: [
+          {
+            name: PREFIX + 'help',
+            value: 'You´ll see this here'
+          }
+        ],
+        timestamp: new Date()
+      }
+    })
+  }
+})
+
+client.login(TOKEN);
