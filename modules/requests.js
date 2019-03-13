999d2e57a6d7b312b96482808e824b164adb1b71
Julian Yaman <julianyaman@posteo.eu>
2019-03-13 11:17:12 +0100

    wiki cmd: Adding the thumbnail property to the embed Now it shows the Wikipedia thumbnail picture of the searched article

--- modules/requests.js
+++ modules/requests.js
@@ -16,17 +16,13 @@ const Util = require('./util')
  * */
 exports.getWikipediaShortSummary = (msg, argument) => {
 
+  // Beginning with the sequence of requests for the wikipedia article
   got("https://en.wikipedia.org/w/api.php?action=query&prop=extracts&format=json&exintro=&exsentences=2&titles=" + argument).then(res => {
     try {
       let pageContent = JSON.parse(res.body).query.pages
       let keys = Object.keys(pageContent)
 
       let summary
-
-      // console.log(summary);
-      // console.log("-----");
-      // let stringSplitting = pageContent[keys[0]].extract.toString().match(/([^\.!\?]+[\.!\?]+)|([^\.!\?]+$)/g);
-      // console.log(stringSplitting[0] + stringSplitting[1]);
 
       // Replacing all HTML Tags included in the text
       summary = pageContent[keys[0]].extract.replace(/<(?:.|\n)*?>/gm, '')
@@ -42,30 +38,51 @@ exports.getWikipediaShortSummary = (msg, argument) => {
           // Get the value of the fullurl parameter
           let wikipediaArticleLink = pageObject[key[0]].fullurl
 
-          // Sending the final result of the two requests as an embed to the channel where the command
-          // was executed.
-          msg.channel.send({
-            embed: {
-              color: 3447003,
-              author: {
-                icon_url: 'https://upload.wikimedia.org/wikipedia/commons/6/63/Wikipedia-logo.png',
-                name: 'Wikipedia'
-              },
-              title: pageContent[keys[0]].title + ' (wikipedia article)',
-              url: wikipediaArticleLink,
-              description: summary,
-              timestamp: new Date(),
-              footer: {
-                icon_url: 'https://upload.wikimedia.org/wikipedia/commons/6/63/Wikipedia-logo.png',
-                text: 'Information by Wikipedia. wikipedia.org'
-              }
+          // Finalizing the result and now requesting the thumbnail of the wikipedia article by the Media Wikipedia API.
+          got("https://en.wikipedia.org/api/rest_v1/page/media/" + argument).then(res => {
+            try {
+
+              let mediaJSON = JSON.parse(res.body).items[0]
+              let thumbnailSource = mediaJSON.thumbnail.source
+
+              // Sending the final result of the two requests as an embed to the channel where the command
+              // was executed.
+              msg.channel.send({
+                embed: {
+                  color: 3447003,
+                  author: {
+                    icon_url: 'https://upload.wikimedia.org/wikipedia/commons/6/63/Wikipedia-logo.png',
+                    name: 'Wikipedia'
+                  },
+                  title: pageContent[keys[0]].title + ' (wikipedia article)',
+                  url: wikipediaArticleLink,
+                  description: summary,
+                  timestamp: new Date(),
+                  thumbnail: {
+                    url: thumbnailSource
+                  },
+                  footer: {
+                    icon_url: 'https://upload.wikimedia.org/wikipedia/commons/6/63/Wikipedia-logo.png',
+                    text: 'Information by Wikipedia. wikipedia.org'
+                  }
+                }
+              })
+
+            }catch (e) {
+              msg.react('⛔')
+              msg.channel.send(
+                'You got a very rare error here, how did you get that? Write it to our GitHub Repository\n' +
+                'https://github.com/julianYaman/wikipedia-bot')
+              Util.betterError(msg, e)
             }
+
           })
         } catch (e) {
           msg.react('⛔')
           msg.channel.send(
             'You got a very rare error here, how did you get that? Write it to our GitHub Repository\n' +
             'https://github.com/julianYaman/wikipedia-bot')
+          Util.betterError(msg, e)
         }
       })
     } catch (e) {
