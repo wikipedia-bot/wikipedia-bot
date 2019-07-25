f6eaa48d1d840e4e3a646f6bd49b62b7042aedae
Julian Yaman <julianyaman@posteo.eu>
2019-07-25 02:46:44 +0200

    changed log info message @ wiki command

--- commands/wiki.js
+++ commands/wiki.js
@@ -13,10 +13,10 @@ module.exports = {
     // Check in what type of channel the command was executed
     if(message.channel.type === "dm" || message.channel.type === "group"){
       // If it was in a dm or in a group dm, then log only that it was used in a DM channel without logging anything related to the user.
-      Util.log(`${config.PREFIX + this.name} (args: [${args}]) used in a private ${message.channel.type}.`, `Check log for any incoming errors for fixing new bugs!`)
+      Util.log(`${config.PREFIX + this.name} (args: [${args}]) used in a private ${message.channel.type}.`, `Feature: Wiki cmd`)
     }else{
       // If it was somewhere else, then log normally like before.
-      Util.log(`${config.PREFIX + this.name} (args: [${args}]) used on ${message.guild.name} (${message.guild.id})`, `Check log for any incoming errors for fixing new bugs!`)
+      Util.log(`${config.PREFIX + this.name} (args: [${args}]) used on ${message.guild.name} (${message.guild.id})`, `Feature: Wiki cmd`)
     }
 
     if (!args[1]) {
