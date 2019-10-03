290b7cc9b01664756f83dc5010f6c8f953d3bf6d
Julian Yaman <julianyaman@posteo.eu>
2019-10-03 21:25:07 +0200

    fix(wiki-command): log will show the command which was used by the user and not the name of the main command

--- commands/wiki.js
+++ commands/wiki.js
@@ -28,10 +28,10 @@ module.exports = {
     // Check in what type of channel the command was executed
     if(message.channel.type === "dm" || message.channel.type === "group"){
       // If it was in a dm or in a group dm, then log only that it was used in a DM channel without logging anything related to the user.
-      Util.log(`${config.PREFIX + this.name} (args: [${args}]) used in a private ${message.channel.type}.`, `Feature: Wiki cmd`)
+      Util.log(`${config.PREFIX + command} (args: [${args}]) used in a private ${message.channel.type}.`, `Feature: Wiki cmd`)
     }else{
       // If it was somewhere else, then log normally like before.
-      Util.log(`${config.PREFIX + this.name} (args: [${args}]) used on ${message.guild.name} (${message.guild.id})`, `Feature: Wiki cmd`)
+      Util.log(`${config.PREFIX + command} (args: [${args}]) used on ${message.guild.name} (${message.guild.id})`, `Feature: Wiki cmd`)
     }
 
     if (!args[1]) {
