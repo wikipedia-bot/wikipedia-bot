f3054d4db36fba2bda0178be9c9422c569d4a197
Julian Yaman <julianyaman@posteo.eu>
2020-03-18 17:02:46 +0100

    feat(Util): Replaced all single log functions to one combined Logger class

--- modules/util.js
+++ modules/util.js
@@ -16,70 +16,38 @@ colors.setTheme({
 /**
  * Returns a rounded number.
  *
- * @param value - The number you want to round.
- * @param precision - Precision of the decimal number.
+ * @param {Number} value - The number you want to round.
+ * @param {Number} precision - Precision of the decimal number.
  *
  * @private
  */
 // Thanks Billy Moon for giving the answer how to make a more precise round function: https://stackoverflow.com/a/7343013
-exports.roundNumber = (/** Number */ value, /** Integer */ precision) => {
+exports.roundNumber = (value, precision) => {
 	const multiplier = Math.pow(10, precision || 0)
 	return Math.round(value * multiplier) / multiplier
 }
 
-/**
- * Better solution for logging Discord-related errors
- *
- * @param msg - Message object
- * @param text - Text which should be sent in the console
- *
- * @public
- *
- */
-exports.betterError = (/** Message*/msg, /** String*/text) => {
-	if(msg.channel.type === 'dm' || msg.channel.type === 'group') {
-		// If it was in a dm or in a group dm, then log only that it was used in a DM channel without logging anything related to the user.
-		console.log(`[ERR] [${dateTime({ local: true, showTimeZone: true })}] Private Direct Message Channel -> ${text}`.error)
+exports.Logger = class {
+
+	debug(text) {
+		console.log(`[DEBUG] [${dateTime({ local: true, showTimeZone: true })}] ${text}`.debug)
 	}
-	else{
-		// If it was somewhere else, then log normally like before.
-		console.log(`[ERR] [${dateTime({ local: true, showTimeZone: true })}] (${msg.guild.name} | ${msg.guild.id}) -> ${text}`.error)
-	}
-	console.log('Raw error log:'.underline.error)
-	console.log(text)
-}
 
-// TODO: Creating a function which logs normal information for checking if everything runs good and
-//  for logging errors which are not related to Discord but e.g. to requests.
-
-// Beginning of such an normal information log
-exports.log = (/** String*/text, optionalInput = 'info', type = 0, rawOutput = undefined) => {
-
-	// Types:
-	// 0 - information
-	// 1 - warning
-	// err - error -> use Util.betterError
-	// 2 - debug
-
-	switch (type) {
-	case 0:
-		console.log(`[INFO] [${dateTime({ local: true, showTimeZone: true })}] (${optionalInput}) -> ${text}`.info)
-		break
-	case 1:
-		console.log(`[WARN] [${dateTime({ local: true, showTimeZone: true })}] (${optionalInput}) -> ${text}`.warn)
-		break
-	case 2:
-		console.log(`[DEBUG] [${dateTime({ local: true, showTimeZone: true })}] (${optionalInput}) -> ${text}`.debug)
-		break
-	case 'err':
-		console.log(`[ERR] [${dateTime({ local: true, showTimeZone: true })}] (${optionalInput}) -> ${text}`.error)
-		if(rawOutput !== undefined) {
-			console.log(rawOutput)
+	error(msg, text) {
+		if(msg.channel.type === 'dm' || msg.channel.type === 'group') {
+			console.log(`[ERR] [${dateTime({ local: true, showTimeZone: true })}] DM Chat: ${text}`.error)
 		}
-		break
-	default:
-		console.log(`[??] [${dateTime({ local: true, showTimeZone: true })}] (${optionalInput}) -> ${text}`)
+		else{
+			console.log(`[ERR] [${dateTime({ local: true, showTimeZone: true })}] (${msg.guild.name} | ${msg.guild.id}): ${text}`.error)
+		}
 	}
 
+	info(text) {
+		console.log(`[INFO] [${dateTime({ local: true, showTimeZone: true })}] ${text}`.info)
+	}
+
+	warn(text) {
+		console.log(`[WARN] [${dateTime({ local: true, showTimeZone: true })}] ${text}`.warn)
+	}
 
 }
