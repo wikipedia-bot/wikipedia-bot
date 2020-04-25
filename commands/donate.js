47858c20135d9fb8e6576a1f1b0cd18306667979
Julian Yaman <julianyaman@posteo.eu>
2020-04-25 15:45:19 +0200

    feat(donate-cmd): added seperate donate command

--- commands/donate.js
+++ commands/donate.js
@@ -2,13 +2,13 @@ const Util = require('./../modules/util')
 const Logger = new Util.Logger();
 
 /**
- * Command: help
- * Description: The help command. Shows a full list of commands.
+ * Command: donate
+ * Description: Sending an embed with links to PayPal Donation and Patreon
  * */
 module.exports = {
-	name: 'help',
-	alias: ['invite', 'bot'],
-	description: 'The help command shows a full list of all commands.',
+	name: 'donate',
+	alias: ['patreon'],
+	description: '',
 	execute(message, args, config) {
 		// Check in what type of channel the command was executed
 		if(message.channel.type === 'dm' || message.channel.type === 'group') {
@@ -24,24 +24,12 @@ module.exports = {
 				title: message.client.user.username,
 				fields: [
 					{
-						name: 'Commands',
-						value: '[Click here](https://www.julianyaman.de/#commands) to see all commands.',
-					},
-					{
-						name: 'Invite',
-						value: '[Click here](https://discordapp.com/oauth2/authorize?client_id=554751047030013953&scope=bot&permissions=3467328) to add the bot to your server.',
-					},
-					{
 						name: 'Donate',
 						value: 'You can [donate here via PayPal](https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=SJHABMNBYCTBC&source=url). Thank you very much for your help :)',
 					},
 					{
 						name: 'Patreon',
 						value: 'Become a Patron [here](https://www.patreon.com/yaman071).',
-					},
-					{
-						name: 'Feedback & Support',
-						value: 'Join the [Discord Server](https://discord.gg/yAUmDNb) to get support if you\'re having problems with using the bot.',
 					},
 				],
 			},
