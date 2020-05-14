aaccb3b60d8dc1353414a0ee28396e38a47520aa
Julian Yaman <julianyaman@posteo.eu>
2020-05-14 11:45:51 +0200

    feat(info-cmd): new values and information added

--- commands/info.js
+++ commands/info.js
@@ -9,7 +9,7 @@ const bot = require('../bot')
 module.exports = {
 	name: 'info',
 	description: 'Gives you some information about the bot.',
-	execute(message, args, config) {
+	async execute(message, args, config) {
 		// Check in what type of channel the command was executed
 		if(message.channel.type === 'dm' || message.channel.type === 'group') {
 			Logger.info(`${config.PREFIX + this.name} used in a private ${message.channel.type}.`)
@@ -18,38 +18,67 @@ module.exports = {
 			Logger.info(`${config.PREFIX + this.name} used on ${message.guild.name} (${message.guild.id}; ${message.guild.memberCount} users)`)
 		}
 
-		message.channel.send({
+		await message.channel.send({
 			embed: {
 				author: {
 					name: message.client.user.username,
 					icon_url: 'https://cdn.discordapp.com/avatars/554751047030013953/29bda61e7319dd2b49961313b1256231.png',
 					url: 'https://julianyaman.de',
 				},
-				description: `*If you need help, type **${config.PREFIX}help***`,
+				description: `Need help? Type **${config.PREFIX}help**`,
 				color: 3447003,
 				fields: [
 					{
-						name: 'Website:',
-						value: 'https://www.julianyaman.de/',
-						inline: false,
-					},
-					{
 						name: 'Servers',
-						value: message.client.guilds.cache.size,
+						value: await bot.guildCount(),
 						inline: true,
 					},
 					{
 						name: 'Users',
-						value: bot.totalMembers(),
+						value: await bot.totalMembers(),
+						inline: true,
+					},
+					{
+						name: 'Website',
+						value: '[julianyaman.de](https://www.julianyaman.de/)',
+						inline: true,
+					},
+					{
+						name: 'Owner',
+						value: 'yaman#8901',
 						inline: true,
 					},
 					{
 						name: 'Version',
 						value: config.VERSION,
-						inline: false,
+						inline: true,
+					},
+					{
+						name: 'Discord',
+						value: '[Join](https://discord.gg/ccpgH3b)',
+						inline: true,
+					},
+					{
+						name: 'Used library',
+						value: 'discord.js',
+						inline: true,
+					},
+					{
+						name: 'Invite the bot',
+						value: '[here](https://discordapp.com/oauth2/authorize?client_id=554751047030013953&scope=bot&permissions=3467328)',
+						inline: true,
+					},
+					{
+						name: 'GitHub',
+						value: '[Repository](https://github.com/julianYaman/wikipedia-bot)',
+						inline: true,
+					},
+					{
+						name: 'Donate',
+						value: '[via PayPal](https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=SJHABMNBYCTBC&source=url)',
+						inline: true,
 					},
 				],
-				timestamp: new Date(),
 			},
 		})
 	},
