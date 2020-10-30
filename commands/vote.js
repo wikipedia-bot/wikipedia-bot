2f4956cda078f85c586ec8d099b1857c16cae0f5
Julian Yaman <julianyaman@posteo.eu>
2020-10-30 19:07:33 +0100

    refactor(vote-cmd): updated direct message content

--- commands/vote.js
+++ commands/vote.js
@@ -7,7 +7,7 @@ const Logger = new Util.Logger();
  * */
 module.exports = {
 	name: 'vote',
-	description: '**SUPPORT US WITH A VOTE:** Vote for the bot on DiscordBots.org.',
+	description: '**SUPPORT US WITH A VOTE:** Vote for the bot on top.gg.',
 	execute(message, args, config) {
 		// Check in what type of channel the command was executed
 		if(message.channel.type === 'dm' || message.channel.type === 'group') {
@@ -19,10 +19,9 @@ module.exports = {
 
 		if(message.deletable) message.delete();
 
-		message.author.send('Hi, I\'m the developer of this bot you are using right now! Thank you for supporting the effort behind this project.\n\n' +
-      'With your help, the bot will reach a much greater popularity and will be used by more people.\n' +
-      'Please vote on one or on all of the three sites:\n* https://top.gg/bot/554751047030013953/vote **(recommended)** \n* https://discordbotlist.com/bots/554751047030013953/upvote \n* https://bots.ondiscord.xyz/bots/554751047030013953  \n\n' +
-      'Thank you very much! :)\n***~Julian | yaman#8901***')
+		message.author.send('With your help, the bot will reach a much greater popularity and will be used by more people.\n' +
+      'Please vote on one or on all of the three sites:\n* https://top.gg/bot/554751047030013953/vote **(recommended)**\n* https://bots.ondiscord.xyz/bots/554751047030013953  \n* https://discordbotlist.com/bots/554751047030013953/upvote \n\n' +
+      'Thank you very much! :)\n**~yaman#0001**')
 
 	},
 }
